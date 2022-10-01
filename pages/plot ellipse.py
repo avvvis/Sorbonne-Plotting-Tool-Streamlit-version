ed87034d5f08d8bccab890fe208aef9022c640e3
avvvis <110105685+avvvis@users.noreply.github.com>
2022-10-01 16:48:52 +0200

    Update plot ellipse.py

--- pages/plot ellipse.py
+++ pages/plot ellipse.py
@@ -5,8 +5,10 @@ from matplotlib.axes import*
 from math import*
 import numpy as np
 import streamlit as st
+import streamlit.components.v1 as components
 
 
+# creating points of ellipse before change
 def make_ellipse(xc, yc, a, b):
     a = float(a)
     b = float(b)
@@ -21,9 +23,10 @@ def make_ellipse(xc, yc, a, b):
     y_coords = np.array(y_coords)
     return x_coords, y_coords
 
+
+# plotting ellipse after change with animation
 def make_ellipse_changed(var, eq):
     var = variables.split("\n")
-    var.remove('')
     v = {}
     for i in var:
         i = i.split("=")
@@ -32,17 +35,19 @@ def make_ellipse_changed(var, eq):
         v[i[0]] = i[1]
 
     eq = equations.split("\n")
-    eq.remove('')
     e = {}
-    for i in equations:
+    for i in eq:
         i = i.split("=")
         i[0] = i[0].replace(" ", "")
         i[1] = i[1].replace(" ", "")
         e[i[0]] = i[1]
 
-    ani.animation(ff.ellipse(xc, yc, a, b, 100), e, v, grid_cb, 120, 120, 0, 10)
+    circle_ani = ani.animation(ff.ellipse(xc, yc, a, b, 100), e, v, grid_cb, 120, 120, 0, 5)
+    # following line shows animation on the website
+    components.html(circle_ani.to_jshtml(), height=1000)
 
 
+# -front end-
 st.subheader("input change equations")
 equations = st.text_area(label="equations:", value="x = x + sin(t)\ny = y + cos(t)")
 variables = st.text_area(label="variables:", value="alpha = 0")
@@ -58,12 +63,16 @@ st.image(image="https://www.mathsisfun.com/geometry/images/ellipse-axes.svg")
 a = st.slider(label="a", min_value=0, max_value=100, value=10, step=1)
 b = st.slider(label="b", min_value=0, max_value=100, value=10, step=1)
 
-st.subheader("figure before change")
+st.subheader("Change animation:")
+
+# -figure plotting-
 fig, ax = subplots()
 points = make_ellipse(xc, yc, a, b)
 
 # plotting figure before change
 plot = ax.plot(points[0], points[1])
+
+# setting the plot
 gca().set_aspect('equal', adjustable='box')
 xlim([-120, 120])
 ylim([-120, 120])
@@ -72,7 +81,8 @@ yticks(range(-120, 121, 10))
 setp(ax.get_xticklabels()[1::2], visible=False)
 setp(ax.get_yticklabels()[1::2], visible=False)
 
-grid()
-st.pyplot(fig)
+# plotting figure after change with animation
+make_ellipse_changed(variables, equations)
+
 
 
